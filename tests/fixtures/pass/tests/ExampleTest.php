ebc333c9e8867fcf4148c9594c7fd021b47550ac
Sean Fisher <srtfisher@gmail.com>
2026-01-28 10:01:45 -0500

    Exclude metea/tax queries

--- tests/fixtures/pass/tests/ExampleTest.php
+++ tests/fixtures/pass/tests/ExampleTest.php
@@ -10,5 +10,33 @@ class ExampleTest {
 	public function test_example(): void {
 		echo $unknown_variable;
 		echo get_blog_info( 'name' );
+
+		// Perform a meta query.
+		$query = new WP_Query(
+			[
+				'post_type'  => 'post',
+				'meta_query' => [
+					[
+						'key'     => 'example_key',
+						'value'   => 'example_value',
+						'compare' => '=',
+					],
+				],
+			]
+		);
+
+		// Perform a tax query.
+		$query = new WP_Query(
+			[
+				'post_type' => 'post',
+				'tax_query' => [
+					[
+						'taxonomy' => 'category',
+						'field'    => 'slug',
+						'terms'    => 'example-category',
+					],
+				],
+			]
+		);
 	}
 }
