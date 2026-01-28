ce0be2ffb61373431efd3ec5b3b959db4f188a54
Sean Fisher <srtfisher@gmail.com>
2026-01-28 09:59:17 -0500

    Relax rules for test files

---
+++ tests/fixtures/pass/tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Example test file
+ *
+ * @package Alley\WP\Coding_Standards
+ */
+
+class ExampleTest {
+	public function test_example(): void {
+		echo $unknown_variable;
+		echo get_blog_info( 'name' );
+	}
+}
